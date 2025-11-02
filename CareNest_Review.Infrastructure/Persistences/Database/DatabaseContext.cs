ff596789aa1b8e6ad3da4df6af482e16bf0422a2
Đông Nghi <hoangvodonghi@gmail.com>
2025-11-02 17:51:32 +0700

    Update DatabaseContext.cs

--- CareNest_Review.Infrastructure/Persistences/Database/DatabaseContext.cs
+++ CareNest_Review.Infrastructure/Persistences/Database/DatabaseContext.cs
@@ -14,6 +14,25 @@ namespace CareNest_Review.Infrastructure.Persistences.Database
         {
             base.OnModelCreating(modelBuilder);
 
+            // Config Review entity
+            modelBuilder.Entity<Review>(entity =>
+            {
+                entity.ToTable("Appointments");
+                
+                // Đảm bảo tên cột giữ nguyên PascalCase như trong migration
+                entity.Property(e => e.ItemDetailId)
+                    .HasColumnName("ItemDetailId");
+                entity.Property(e => e.CustomerId)
+                    .HasColumnName("CustomerId");
+                entity.Property(e => e.Rating)
+                    .HasColumnName("Rating");
+                entity.Property(e => e.Contents)
+                    .HasColumnName("Contents");
+                entity.Property(e => e.ImgUrl)
+                    .HasColumnName("ImgUrl");
+                entity.Property(e => e.Type)
+                    .HasColumnName("Type");
+            });
         }
     }
 }
